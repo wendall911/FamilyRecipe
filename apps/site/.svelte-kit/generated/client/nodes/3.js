12631d5c52fea1863220c7f1849bf603b71196a9
Wendall Cada <wendallc@83864.com>
2026-07-17 15:13:44 -0700

    Remove dev files committed by accident.

--- apps/site/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/recipe/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/recipe/[slug]/+page.svelte";
