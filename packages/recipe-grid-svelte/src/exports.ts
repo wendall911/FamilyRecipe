6eabe01bcf8c90dcc63c0e3af8429e0729653f3c
Wendall Cada <wendallc@83864.com>
2026-07-22 17:09:03 -0700

    Export StructureNode so types work.

--- packages/recipe-grid-svelte/src/exports.ts
+++ packages/recipe-grid-svelte/src/exports.ts
@@ -2,6 +2,7 @@ export { default as Root } from './components/RecipeRoot.svelte';
 export { default as Grid } from './components/RecipeGrid.svelte';
 export { default as Title } from './components/RecipeTitle.svelte';
 export { default as Description } from './components/RecipeDescription.svelte';
+export type { StructureNode } from '@wendall911/recipe-grid';
 
 export type {
     RecipeContext,
