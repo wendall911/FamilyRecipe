009ac627eaade9c30e17d1809c238b7f4c9f8a08
Wendall Cada <wendallc@83864.com>
2026-07-24 07:27:17 -0700

    Expose RecipeContext to API surface of recipe-grid-svelte exorts.

--- packages/recipe-grid-svelte/src/exports.ts
+++ packages/recipe-grid-svelte/src/exports.ts
@@ -2,8 +2,8 @@ export { default as Root } from './components/RecipeRoot.svelte';
 export { default as Grid } from './components/RecipeGrid.svelte';
 export { default as Title } from './components/RecipeTitle.svelte';
 export { default as Description } from './components/RecipeDescription.svelte';
+export { RecipeContext } from './recipe.js';
 export type { StructureNode } from '@wendall911/recipe-grid';
 
 export type {
-    RecipeContext,
 } from './types.js';
--- packages/recipe-grid-svelte/src/types.ts
+++
@@ -1,1 +0,0 @@
-export type { RecipeContext } from './recipe.js';
