afea41b739de8b2e346c7eca803b917a02989a60
Wendall Cada <wendallc@83864.com>
2026-08-03 10:18:30 -0700

    export RecipeParseError so it can be rethrown.

--- packages/recipe-grid-svelte/src/exports.ts
+++ packages/recipe-grid-svelte/src/exports.ts
@@ -3,6 +3,7 @@ export { default as Grid } from './components/RecipeGrid.svelte';
 export { default as Title } from './components/RecipeTitle.svelte';
 export { default as Description } from './components/RecipeDescription.svelte';
 export { RecipeContext } from './recipe.js';
+export { RecipeParseError } from '@wendall911/recipe-grid';
 export type { StructureNode } from '@wendall911/recipe-grid';
 
 export type {
